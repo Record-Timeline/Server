ce0440b0a915f9717c6791158d8513bb3943bb3f
hyeonbin <basilla0617@naver.com>
2024-03-22 16:01:58 +0900

    Initial Commit

---
+++ src/main/java/com/api/RecordTimeline/RecordTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.api.RecordTimeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordTimelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/RecordTimeline/RecordTimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.RecordTimeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordTimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
