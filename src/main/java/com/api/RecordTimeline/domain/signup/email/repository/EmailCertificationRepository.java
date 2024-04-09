6d07607a1d20db628102fefeb4bc2d6a4cf91e81
hyeonbin <basilla0617@naver.com>
2024-04-09 20:54:42 +0900

    feat: EmailProvider 및 EmailCertificationRepository 구현

---
+++ src/main/java/com/api/RecordTimeline/domain/signup/email/repository/EmailCertificationRepository.java
@@ -0,0 +1,8 @@
+package com.api.RecordTimeline.domain.signup.email.repository;
+
+import com.api.RecordTimeline.domain.signup.email.domain.EmailCertification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailCertificationRepository extends JpaRepository<EmailCertification,String> {
+    EmailCertification findByMemberId (String memberId);
+}
