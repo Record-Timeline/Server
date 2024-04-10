1a3b4bbbfb446f877ff378d6214dbdd20ba1c4cd
hyeonbin <basilla0617@naver.com>
2024-04-10 15:43:20 +0900

    feat: 파라미터 타입 변경 및 email을 사용한 메서드를 memberId 사용으로 변경

--- src/main/java/com/api/RecordTimeline/domain/member/repository/MemberRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/member/repository/MemberRepository.java
@@ -3,8 +3,6 @@ package com.api.RecordTimeline.domain.member.repository;
 import com.api.RecordTimeline.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-    Optional<Member> findByEmail (String email);
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByMemberId (String memberId);
 }
