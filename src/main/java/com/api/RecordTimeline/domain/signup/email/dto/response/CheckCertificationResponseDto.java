f6e85cac469d65bbfc6b70cb87363983f567ff69
hyeonbin <basilla0617@naver.com>
2024-04-12 02:04:35 +0900

    feat: memberNotFound ResponseEntity 설정

--- src/main/java/com/api/RecordTimeline/domain/signup/email/dto/response/CheckCertificationResponseDto.java
+++ src/main/java/com/api/RecordTimeline/domain/signup/email/dto/response/CheckCertificationResponseDto.java
@@ -17,6 +17,11 @@ public class CheckCertificationResponseDto extends ResponseDto {
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
+    public static ResponseEntity<ResponseDto> memberNotFound() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.MEMBER_NOT_FOUND, ResponseMessage.MEMBER_NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+    }
+
     public static ResponseEntity<ResponseDto> certificationFail() {
         ResponseDto responseBody = new ResponseDto(ResponseCode.CERTIFICATION_FAILED, ResponseMessage.CERTIFICATION_FAILED);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
