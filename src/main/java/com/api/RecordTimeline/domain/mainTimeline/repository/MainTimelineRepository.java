8b3bff18d7443f2c4b0204be8f77c105eb9025b3
dhdbrud0510 <youkyungoh0510@naver.com>
2024-04-15 02:09:39 +0900

    feat: 메인타임라인 시작 날짜 기준으로 정렬 후 조회하는 쿼리 메소드 추가

--- src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MainTimelineRepository extends JpaRepository<MainTimeline, Long> {
-    List<MainTimeline> findByMemberId(Long memberId); // 특정 사용자가 생성한 메인 타임라인 조회
+    // 특정 사용자가 생성한 메인 타임라인 조회
+    List<MainTimeline> findByMemberId(Long memberId);
+
+    // 특정 사용자의 메인 타임라인을 시작 날짜 기준으로 정렬하여 조회
+    List<MainTimeline> findByMemberIdOrderByStartDate(Long memberId);
 
 }
-
