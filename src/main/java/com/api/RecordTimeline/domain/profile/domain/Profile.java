835ab58aa3b85a74936f40a59303415026a9dc40
hyeonbin <basilla0617@naver.com>
2024-04-15 22:19:08 +0900

    feat: 프로필 엔티티 구현

--- src/main/java/com/api/RecordTimeline/domain/profile/domain/Profile.java
+++ src/main/java/com/api/RecordTimeline/domain/profile/domain/Profile.java
@@ -5,13 +5,13 @@ import com.api.RecordTimeline.domain.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
 
-/** 연관관계를 매핑을 위해 작성. 완성된 코드 X **/
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Profile extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,4 +19,30 @@ public class Profile extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", unique = true)
     private Member member;
+
+    private String profileImgUrl;
+
+    private String introduction;
+
+    public Profile(Member member, String profileImgUrl, String introduction) {
+        this.member = member;
+        this.profileImgUrl = profileImgUrl;
+        this.introduction = introduction;
+    }
+
+    public static Profile of(final Member member, final String profileImgUrl, final String introduction) {
+        return Profile.builder()
+                .member(member)
+                .profileImgUrl(profileImgUrl)
+                .introduction(introduction)
+                .build();
+    }
+    public void changeProfileImage(String profileImgUrl) {
+        this.profileImgUrl = profileImgUrl;
+    }
+
+    public void deleteProfileImage() {
+        this.profileImgUrl = null;
+    }
+
 }
