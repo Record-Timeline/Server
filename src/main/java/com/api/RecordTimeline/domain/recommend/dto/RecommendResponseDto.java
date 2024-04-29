7ab9f43f8c759603f33fa5df642994e3e75b7950
hyeonbin <basilla0617@naver.com>
2024-04-29 21:45:00 +0900

    feat: 코드 삭제

--- src/main/java/com/api/RecordTimeline/domain/recommend/dto/RecommendResponseDto.java
+++ src/main/java/com/api/RecordTimeline/domain/recommend/dto/RecommendResponseDto.java
@@ -1,17 +1,17 @@
-package com.api.RecordTimeline.domain.recommend.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RecommendResponseDto {
-        private String nickname;
-        private String profileImageUrl;
-        private String introduction;
-
-
-}
+//package com.api.RecordTimeline.domain.recommend.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class RecommendResponseDto {
+//        private String nickname;
+//        private String profileImageUrl;
+//        private String introduction;
+//
+//
+//}
--- src/main/java/com/api/RecordTimeline/domain/recommend/service/RecommendServiceImpl.java
+++ src/main/java/com/api/RecordTimeline/domain/recommend/service/RecommendServiceImpl.java
@@ -1,50 +1,50 @@
-package com.api.RecordTimeline.domain.recommend.service;
-
-import com.api.RecordTimeline.domain.member.domain.Interest;
-import com.api.RecordTimeline.domain.member.domain.Member;
-import com.api.RecordTimeline.domain.member.repository.MemberRepository;
-import com.api.RecordTimeline.domain.profile.domain.Profile;
-import com.api.RecordTimeline.domain.profile.repository.ProfileRepository;
-import com.api.RecordTimeline.domain.recommend.dto.RecommendResponseDto;
-import com.api.RecordTimeline.global.exception.ApiException;
-import com.api.RecordTimeline.global.exception.ErrorType;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-@RequiredArgsConstructor
-public class RecommendServiceImpl implements RecommendService {
-
-    private final MemberRepository memberRepository;
-    private final ProfileRepository profileRepository;
-
-    @Transactional
-    @Override
-    public ResponseEntity<List<RecommendResponseDto>> recommendSameInterestMember(String email) {
-        Member member = memberRepository.findByEmailAndIsDeletedFalse(email);
-        if (member == null) {
-            throw new ApiException(ErrorType._USER_NOT_FOUND_DB);
-        }
-
-        List<Member> membersWithSameInterest = memberRepository.findMembersWithSameInterest(member.getInterest(), email);
-        Collections.shuffle(membersWithSameInterest);
-        List<Member> selectedMembers = membersWithSameInterest.stream().limit(5).collect(Collectors.toList());
-
-        List<RecommendResponseDto> result = selectedMembers.stream().map(m -> {
-            Profile profile = profileRepository.findByMemberAndIsDeletedFalse(m);
-            return new RecommendResponseDto(
-                    m.getNickname(),
-                    profile != null ? profile.getProfileImgUrl() : "",
-                    profile != null ? profile.getIntroduction() : ""
-            );
-        }).collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
-    }
-}
+//package com.api.RecordTimeline.domain.recommend.service;
+//
+//import com.api.RecordTimeline.domain.member.domain.Interest;
+//import com.api.RecordTimeline.domain.member.domain.Member;
+//import com.api.RecordTimeline.domain.member.repository.MemberRepository;
+//import com.api.RecordTimeline.domain.profile.domain.Profile;
+//import com.api.RecordTimeline.domain.profile.repository.ProfileRepository;
+//import com.api.RecordTimeline.domain.recommend.dto.RecommendResponseDto;
+//import com.api.RecordTimeline.global.exception.ApiException;
+//import com.api.RecordTimeline.global.exception.ErrorType;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RecommendServiceImpl implements RecommendService {
+//
+//    private final MemberRepository memberRepository;
+//    private final ProfileRepository profileRepository;
+//
+//    @Transactional
+//    @Override
+//    public ResponseEntity<List<RecommendResponseDto>> recommendSameInterestMember(String email) {
+//        Member member = memberRepository.findByEmailAndIsDeletedFalse(email);
+//        if (member == null) {
+//            throw new ApiException(ErrorType._USER_NOT_FOUND_DB);
+//        }
+//
+//        List<Member> membersWithSameInterest = memberRepository.findMembersWithSameInterest(member.getInterest(), email);
+//        Collections.shuffle(membersWithSameInterest);
+//        List<Member> selectedMembers = membersWithSameInterest.stream().limit(5).collect(Collectors.toList());
+//
+//        List<RecommendResponseDto> result = selectedMembers.stream().map(m -> {
+//            Profile profile = profileRepository.findByMemberAndIsDeletedFalse(m);
+//            return new RecommendResponseDto(
+//                    m.getNickname(),
+//                    profile != null ? profile.getProfileImgUrl() : "",
+//                    profile != null ? profile.getIntroduction() : ""
+//            );
+//        }).collect(Collectors.toList());
+//
+//        return ResponseEntity.ok(result);
+//    }
+//}
