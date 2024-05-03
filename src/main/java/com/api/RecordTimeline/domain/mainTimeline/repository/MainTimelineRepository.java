26812ba322151d6e9fa482866a1cfd6ee2a59d2d
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-04 01:07:57 +0900

    feat: 시작 날짜 정렬 쿼리 추가

--- src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
@@ -2,6 +2,7 @@ package com.api.RecordTimeline.domain.mainTimeline.repository;
 
 import com.api.RecordTimeline.domain.mainTimeline.domain.MainTimeline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ public interface MainTimelineRepository extends JpaRepository<MainTimeline, Long
     List<MainTimeline> findByMemberId(Long memberId);
 
     // 특정 사용자의 메인 타임라인을 시작 날짜 기준으로 정렬하여 조회
+    @Query("select m from MainTimeline m where m.member.id = ?1 order by m.startDate")
     List<MainTimeline> findByMemberIdOrderByStartDate(Long memberId);
 
 }
