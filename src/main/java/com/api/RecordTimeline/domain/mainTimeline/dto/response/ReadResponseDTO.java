0092777d54afb09649e1b2e9f87f40189f6561b3
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-04 18:27:08 +0900

    feat: 메인타임라인 ReadResponseDTO 추가

---
+++ src/main/java/com/api/RecordTimeline/domain/mainTimeline/dto/response/ReadResponseDTO.java
@@ -0,0 +1,30 @@
+package com.api.RecordTimeline.domain.mainTimeline.dto.response;
+
+import com.api.RecordTimeline.domain.mainTimeline.domain.MainTimeline;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class ReadResponseDTO {
+    private List<TimelineDetails> timelines;
+
+    @Getter
+    @AllArgsConstructor
+    public static class TimelineDetails {
+        private Long id;
+        private String title;
+        private LocalDate startDate;
+        private LocalDate endDate;
+    }
+
+    public static ReadResponseDTO from(List<MainTimeline> timelines) {
+        List<TimelineDetails> details = timelines.stream()
+                .map(timeline -> new TimelineDetails(timeline.getId(), timeline.getTitle(), timeline.getStartDate(), timeline.getEndDate()))
+                .collect(Collectors.toList());
+        return new ReadResponseDTO(details);
+    }
+}
