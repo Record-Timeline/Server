5c294252e27f082bca1db43eeaac314c982f8c3b
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-10 04:02:12 +0900

    feat: 서브타임라인 컨트롤러 생성

---
+++ src/main/java/com/api/RecordTimeline/domain/subTimeline/controller/SubTimelineController.java
@@ -0,0 +1,32 @@
+package com.api.RecordTimeline.domain.subTimeline.controller;
+
+import com.api.RecordTimeline.domain.subTimeline.domain.SubTimeline;
+import com.api.RecordTimeline.domain.subTimeline.service.SubTimelineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sub-timelines")
+public class SubTimelineController {
+    @Autowired
+    private SubTimelineService subTimelineService;
+
+    @PostMapping
+    public ResponseEntity<SubTimeline> createSubTimeline(@RequestBody SubTimeline subTimeline) {
+        SubTimeline created = subTimelineService.createSubTimeline(subTimeline);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/main/{mainTimelineId}")
+    public ResponseEntity<List<SubTimeline>> getSubTimelinesByMainTimelineId(@PathVariable Long mainTimelineId) {
+        List<SubTimeline> subTimelines = subTimelineService.getSubTimelinesByMainTimelineId(mainTimelineId);
+        return ResponseEntity.ok(subTimelines);
+    }
+
+    // 추가적인 엔드포인트 구현
+}
+
