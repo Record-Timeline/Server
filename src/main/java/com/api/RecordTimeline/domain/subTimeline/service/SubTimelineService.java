91a20109aa86989152721fdffe6a195600e525e3
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-12 01:46:08 +0900

    feat: s3FileUploader 추가

--- src/main/java/com/api/RecordTimeline/domain/subTimeline/service/SubTimelineService.java
+++ src/main/java/com/api/RecordTimeline/domain/subTimeline/service/SubTimelineService.java
@@ -1,25 +1,43 @@
 package com.api.RecordTimeline.domain.subTimeline.service;
 
+import com.api.RecordTimeline.domain.mainTimeline.domain.MainTimeline;
+import com.api.RecordTimeline.domain.mainTimeline.repository.MainTimelineRepository;
 import com.api.RecordTimeline.domain.subTimeline.domain.SubTimeline;
+import com.api.RecordTimeline.domain.subTimeline.dto.SubTimelineCreateRequest;
 import com.api.RecordTimeline.domain.subTimeline.repository.SubTimelineRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.api.RecordTimeline.global.s3.S3FileUploader;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SubTimelineService {
-    @Autowired
-    private SubTimelineRepository subTimelineRepository;
+    private final SubTimelineRepository subTimelineRepository;
+    private final MainTimelineRepository mainTimelineRepository;
+    private final S3FileUploader s3FileUploader;
 
-    public SubTimeline createSubTimeline(SubTimeline subTimeline) {
+    public SubTimelineService(SubTimelineRepository subTimelineRepository, MainTimelineRepository mainTimelineRepository, S3FileUploader s3FileUploader) {
+        this.subTimelineRepository = subTimelineRepository;
+        this.mainTimelineRepository = mainTimelineRepository;
+        this.s3FileUploader = s3FileUploader;
+    }
+
+    public SubTimeline createSubTimeline(SubTimelineCreateRequest request) {
+        MainTimeline mainTimeline = mainTimelineRepository.findById(request.getMainTimelineId())
+                .orElseThrow(() -> new IllegalArgumentException("Main timeline not found with ID: " + request.getMainTimelineId()));
+        List<String> imageUrls = s3FileUploader.uploadMultipartFiles(request.getImages());
+        SubTimeline subTimeline = SubTimeline.builder()
+                .mainTimeline(mainTimeline)
+                .title(request.getTitle())
+                .content(request.getContent())
+                .startDate(request.getStartDate())
+                .endDate(request.getEndDate())
+                .imageUrls(imageUrls)
+                .build();
         return subTimelineRepository.save(subTimeline);
     }
 
     public List<SubTimeline> getSubTimelinesByMainTimelineId(Long mainTimelineId) {
         return subTimelineRepository.findByMainTimelineId(mainTimelineId);
     }
-
-    // 추가적인 메소드 구현
 }
-
