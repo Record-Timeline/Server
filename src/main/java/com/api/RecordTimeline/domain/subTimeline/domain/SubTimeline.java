77728022a6d112abcf4c20506efdc8c8fd6a53aa
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-12 01:43:51 +0900

    feat: startDate, endDate 추가

--- src/main/java/com/api/RecordTimeline/domain/subTimeline/domain/SubTimeline.java
+++ src/main/java/com/api/RecordTimeline/domain/subTimeline/domain/SubTimeline.java
@@ -5,6 +5,7 @@ import com.api.RecordTimeline.domain.mainTimeline.domain.MainTimeline;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /** 연관관계를 매핑을 위해 작성. 완성된 코드 X **/
@@ -13,8 +14,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //JPA 스펙에 맞춘 기본 생성자
 @AllArgsConstructor(access = AccessLevel.PRIVATE) // 빌더를 사용하기 위해 모든 인자를 받는 생성자를 private으로 설정
+@Builder
 public class SubTimeline extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,19 +27,26 @@ public class SubTimeline extends BaseEntity {
     @Column(nullable = false)
     private String title; // 서브 타임라인의 제목
 
-    @Column(nullable = false, length = 1000)
+    @Column(nullable = false)
     private String content; // 서브 타임라인의 내용
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;
 
     @ElementCollection // 간단한 값 목록을 저장할 때 사용
     @CollectionTable(name = "sub_timeline_images", joinColumns = @JoinColumn(name = "sub_timeline_id"))
     @Column(name = "image_url")
     private List<String> imageUrls; // 이미지 URL 목록
 
-    @Builder // 빌더 패턴을 사용하여 객체 생성을 간편하게 처리
     public SubTimeline(MainTimeline mainTimeline, String title, String content, List<String> imageUrls) {
         this.mainTimeline = mainTimeline;
         this.title = title;
         this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.imageUrls = imageUrls;
     }
 }
