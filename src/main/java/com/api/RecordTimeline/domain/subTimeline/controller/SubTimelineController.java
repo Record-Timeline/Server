c49c464e35abf3c2e333d4889cf0e473b47ad590
dhdbrud0510 <youkyungoh0510@naver.com>
2024-05-12 01:44:49 +0900

    feat: PostMapping 수정 @ModelAttribute

--- src/main/java/com/api/RecordTimeline/domain/subTimeline/controller/SubTimelineController.java
+++ src/main/java/com/api/RecordTimeline/domain/subTimeline/controller/SubTimelineController.java
@@ -1,6 +1,7 @@
 package com.api.RecordTimeline.domain.subTimeline.controller;
 
 import com.api.RecordTimeline.domain.subTimeline.domain.SubTimeline;
+import com.api.RecordTimeline.domain.subTimeline.dto.SubTimelineCreateRequest;
 import com.api.RecordTimeline.domain.subTimeline.service.SubTimelineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/sub-timelines")
+@RequestMapping("/api/v1/sub-timelines")
 public class SubTimelineController {
     @Autowired
     private SubTimelineService subTimelineService;
 
-    @PostMapping
-    public ResponseEntity<SubTimeline> createSubTimeline(@RequestBody SubTimeline subTimeline) {
-        SubTimeline created = subTimelineService.createSubTimeline(subTimeline);
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<SubTimeline> createSubTimeline(@ModelAttribute SubTimelineCreateRequest request) {
+        SubTimeline created = subTimelineService.createSubTimeline(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
