ee7fa7e0314bda6203a6bd7a41e80d212bd569ef
hyeonbin <basilla0617@naver.com>
2024-05-23 00:35:48 +0900

    feat: 서브 타임라인 검색 기능 구현

--- src/main/java/com/api/RecordTimeline/domain/search/service/SearchService.java
+++ src/main/java/com/api/RecordTimeline/domain/search/service/SearchService.java
@@ -2,6 +2,8 @@ package com.api.RecordTimeline.domain.search.service;
 
 import com.api.RecordTimeline.domain.member.dto.response.MemberInfoResponseDto;
 import com.api.RecordTimeline.domain.search.dto.response.SearchPageRecommendDto;
+import com.api.RecordTimeline.domain.search.dto.response.SearchResultDto;
+import com.api.RecordTimeline.domain.search.dto.response.SearchSubTimelineDto;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -10,4 +12,8 @@ public interface SearchService {
     ResponseEntity<List<SearchPageRecommendDto>> recommendSameInterestMember(String email);
 
     List<MemberInfoResponseDto> searchMembersByKeyword(String keyword);
+
+    List<SearchSubTimelineDto> searchSubTimelinesByKeyword(String keyword);
+
+    SearchResultDto searchAllByKeyword(String keyword);
 }
