c6af8dd9289ee586ee1ae78d165d9c723bc98ede
hyeonbin <basilla0617@naver.com>
2024-05-25 18:33:43 +0900

    refactor: Java Multiline String을 이용해 쿼리 보기 좋게 수정

--- src/main/java/com/api/RecordTimeline/domain/subTimeline/repository/SubTimelineRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/subTimeline/repository/SubTimelineRepository.java
@@ -15,13 +15,34 @@ public interface SubTimelineRepository extends JpaRepository<SubTimeline, Long> 
     // 서브 타임라인을 시작 날짜 기준으로 정렬하여 조회하는 쿼리
     @Query("select s from SubTimeline s where s.mainTimeline.id = ?1 order by s.startDate")
     List<SubTimeline> findByMainTimelineIdOrderByStartDate(Long mainTimelineId);
-    @Query("SELECT st FROM SubTimeline st JOIN st.mainTimeline mt JOIN mt.member m WHERE (st.title LIKE %:keyword% OR st.content LIKE %:keyword%) AND m.isDeleted = false")
+    @Query("""
+
+            SELECT st FROM SubTimeline st 
+                JOIN st.mainTimeline mt 
+                JOIN mt.member m 
+            WHERE (st.title LIKE %:keyword% OR st.content LIKE %:keyword%) 
+            AND m.isDeleted = false
+            """)
     List<SubTimeline> findByTitleOrContentContaining(@Param("keyword") String keyword);
 
-    @Query(value = "SELECT st.* FROM sub_timeline st JOIN main_timeline mt ON st.main_timeline_id = mt.id JOIN member m ON mt.member_id = m.id WHERE m.interest = :interest AND m.email != :email AND m.is_deleted = false ORDER BY RAND() LIMIT 4", nativeQuery = true)
+    @Query(value = """
+
+            SELECT st.* FROM sub_timeline st JOIN main_timeline mt ON st.main_timeline_id = mt.id 
+                JOIN member m ON mt.member_id = m.id 
+            WHERE m.interest = :interest 
+                AND m.email != :email AND m.is_deleted = false 
+            ORDER BY RAND() LIMIT 4
+            """   , nativeQuery = true)
     List<SubTimeline> findSubTimelinesByInterestExcludingEmail(String interest, String email);
 
-    @Query(value = "SELECT st.* FROM sub_timeline st JOIN main_timeline mt ON st.main_timeline_id = mt.id JOIN member m ON mt.member_id = m.id WHERE m.interest = :interest AND m.is_deleted = false ORDER BY RAND() LIMIT 4", nativeQuery = true)
+    @Query(value = """
+
+            SELECT st.* FROM sub_timeline st JOIN main_timeline mt ON st.main_timeline_id = mt.id 
+                JOIN member m ON mt.member_id = m.id 
+            WHERE m.interest = :interest 
+                AND m.is_deleted = false 
+            ORDER BY RAND() LIMIT 4
+            """, nativeQuery = true)
     List<SubTimeline> findSubTimelinesByInterest(String interest);
 
 }
