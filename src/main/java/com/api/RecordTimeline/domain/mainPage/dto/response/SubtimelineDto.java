a2f54e90c17b967d0a4f56574d854a386cf0e782
hyeonbin <basilla0617@naver.com>
2024-05-28 23:00:15 +0900

    feat: SubtimelineDto 추가

---
+++ src/main/java/com/api/RecordTimeline/domain/mainPage/dto/response/SubtimelineDto.java
@@ -0,0 +1,18 @@
+package com.api.RecordTimeline.domain.mainPage.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubtimelineDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
