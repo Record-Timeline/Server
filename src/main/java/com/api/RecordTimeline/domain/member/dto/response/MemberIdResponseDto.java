d0e109c026d8584162580a00c7cb3228da9f7a13
hyeonbin <basilla0617@naver.com>
2024-06-01 17:25:00 +0900

    feat: MemberId Response Dto 추가

---
+++ src/main/java/com/api/RecordTimeline/domain/member/dto/response/MemberIdResponseDto.java
@@ -0,0 +1,10 @@
+package com.api.RecordTimeline.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberIdResponseDto {
+    private Long memberId;
+}
