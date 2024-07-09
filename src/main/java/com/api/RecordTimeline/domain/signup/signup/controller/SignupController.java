5ef0cc1a2782910928e43a56162a4831f6e455ae
hyeonbin <basilla0617@naver.com>
2024-07-09 20:17:25 +0900

    refactor: sign up 관련 코드 리펙토링 (반환타입 변경 및 각 계층 역할에 맞도록 변경)

--- src/main/java/com/api/RecordTimeline/domain/signup/signup/controller/SignupController.java
+++ src/main/java/com/api/RecordTimeline/domain/signup/signup/controller/SignupController.java
@@ -1,21 +1,16 @@
 package com.api.RecordTimeline.domain.signup.signup.controller;
 
+import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.request.EmailCheckResquestDto;
+import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.request.NicknameCheckResquestDto;
 import com.api.RecordTimeline.domain.signup.duplicateCheck.service.DuplicateCheckService;
 import com.api.RecordTimeline.domain.signup.email.dto.request.CheckCertificationRequestDto;
 import com.api.RecordTimeline.domain.signup.email.dto.request.EmailCertificationRequestDto;
-import com.api.RecordTimeline.domain.signup.email.dto.response.CheckCertificationResponseDto;
-import com.api.RecordTimeline.domain.signup.email.dto.response.EmailCertificationResponseDto;
 import com.api.RecordTimeline.domain.signup.email.service.EmailService;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.BasicSignupRequestDto;
-import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.request.EmailCheckResquestDto;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.KakaoSignupRequestDto;
-import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.request.NicknameCheckResquestDto;
-import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.response.EmailCheckResponseDto;
-import com.api.RecordTimeline.domain.signup.duplicateCheck.dto.response.NicknameCheckResponseDto;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.UnRegisterRequestDto;
-import com.api.RecordTimeline.domain.signup.signup.dto.response.SignupResponseDto;
-import com.api.RecordTimeline.domain.signup.signup.dto.response.UnRegisterResponseDto;
 import com.api.RecordTimeline.domain.signup.signup.service.SignupService;
+import com.api.RecordTimeline.global.success.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -27,11 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -43,72 +34,86 @@ public class SignupController {
 
     @Operation(summary = "아이디 중복 확인", description = "아이디 중복 확인을 합니다.")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "아이디 중복 확인 성공",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/email-check")
-    public ResponseEntity<? super EmailCheckResponseDto> emailCheck (@RequestBody @Valid EmailCheckResquestDto requestBody) {
-        ResponseEntity<? super EmailCheckResponseDto> response = duplicateCheckService.emailCheck(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<Void>> emailCheck (@RequestBody @Valid EmailCheckResquestDto requestBody) {
+        duplicateCheckService.emailCheck(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(null));
     }
 
     @Operation(summary = "닉네임 중복 확인", description = "닉네임 중복 확인을 합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "닉네임 중복 확인 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+                            schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/nickname-check")
-    public ResponseEntity<? super NicknameCheckResponseDto> nicknameCheck (@RequestBody @Valid NicknameCheckResquestDto requestBody) {
-        ResponseEntity<? super NicknameCheckResponseDto> response = duplicateCheckService.nicknameCheck(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<Void>> nicknameCheck (@RequestBody @Valid NicknameCheckResquestDto requestBody) {
+        duplicateCheckService.nicknameCheck(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(null));
     }
 
     @Operation(summary = "자체 서비스 회원가입", description = "레코드 타임라인 자체 서비스 회원가입을 합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "자체 서비스 회원가입 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+                            schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/basic-signup")
-    public ResponseEntity<? super SignupResponseDto> basicSignup (@RequestBody @Valid BasicSignupRequestDto requestBody) {
-        ResponseEntity<? super SignupResponseDto> response = signupService.basicSignup(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<String>> basicSignup (@RequestBody @Valid BasicSignupRequestDto requestBody) {
+        String token = signupService.basicSignup(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(token));
     }
 
     @Operation(summary = "카카오 소셜 회원가입", description = "카카오 소셜 회원가입을 합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "카카오 소셜 회원가입 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+                            schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/kakao-signup")
-    public ResponseEntity<? super SignupResponseDto> kakaoSignup (@RequestBody @Valid KakaoSignupRequestDto requestBody) {
-        ResponseEntity<? super SignupResponseDto> response = signupService.kakaoSignup(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<String>> kakaoSignup (@RequestBody @Valid KakaoSignupRequestDto requestBody) {
+        String token = signupService.kakaoSignup(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(token));
     }
 
     @Operation(summary = "이메일 인증 메일 전송", description = "이메일 인증 메일을 전송합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "이메일 인증 메일 전송 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+                            schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/email-certification")
-    public ResponseEntity<? super EmailCertificationResponseDto> emailCertification (@RequestBody @Valid EmailCertificationRequestDto requestBody) {
-        ResponseEntity<? super EmailCertificationResponseDto> response = emailService.emailCertification(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<Void>> emailCertification (@RequestBody @Valid EmailCertificationRequestDto requestBody) {
+        emailService.emailCertification(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(null));
     }
 
     @Operation(summary = "이메일 인증 번호 확인", description = "이메일 인증 번호가 일치하는지 확인합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "이메일 인증 번호 확인 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = Long.class)))
+                            schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/check-certification")
-    public ResponseEntity<? super CheckCertificationResponseDto> checkCertification (@RequestBody @Valid CheckCertificationRequestDto requestBody) {
-        ResponseEntity<? super CheckCertificationResponseDto> response = emailService.checkCertification(requestBody);
-        return response;
+    public ResponseEntity<SuccessResponse<Void>> checkCertification (@RequestBody @Valid CheckCertificationRequestDto requestBody) {
+        emailService.checkCertification(requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(null));
+    }
+
+    @Operation(summary = "회원 탈퇴", description = "레코드 타임라인에 회원 탈퇴 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 탈퇴 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class)))
+    })
+    @PostMapping("/unregister")
+    public ResponseEntity<SuccessResponse<Void>> unRegister (@RequestBody @Valid UnRegisterRequestDto requestBody) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName(); // 현재 로그인 한 사용자 이메일
+        signupService.unRegister(email, requestBody);
+        return ResponseEntity.ok(new SuccessResponse<>(null));
     }
 }
--- src/main/java/com/api/RecordTimeline/domain/signup/signup/service/SignupServiceImpl.java
+++ src/main/java/com/api/RecordTimeline/domain/signup/signup/service/SignupServiceImpl.java
@@ -4,17 +4,15 @@ import com.api.RecordTimeline.domain.member.domain.Member;
 import com.api.RecordTimeline.domain.member.repository.MemberRepository;
 import com.api.RecordTimeline.domain.profile.domain.Profile;
 import com.api.RecordTimeline.domain.profile.repository.ProfileRepository;
-import com.api.RecordTimeline.domain.signup.email.dto.response.CheckCertificationResponseDto;
 import com.api.RecordTimeline.domain.signup.email.repository.EmailCertificationRepository;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.BasicSignupRequestDto;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.KakaoSignupRequestDto;
-import com.api.RecordTimeline.domain.common.ResponseDto;
 import com.api.RecordTimeline.domain.signup.signup.dto.request.UnRegisterRequestDto;
-import com.api.RecordTimeline.domain.signup.signup.dto.response.SignupResponseDto;
 import com.api.RecordTimeline.domain.signup.signup.dto.response.UnRegisterResponseDto;
+import com.api.RecordTimeline.global.exception.ApiException;
+import com.api.RecordTimeline.global.exception.ErrorType;
 import com.api.RecordTimeline.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -34,82 +32,63 @@ public class SignupServiceImpl implements SignupService {
     private static final String PASSWORD_PATTERN = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$";
 
     @Override
-    public ResponseEntity<? super SignupResponseDto> basicSignup(BasicSignupRequestDto basicDto) {
-
-        String token;
-        try {
-            String email = basicDto.getEmail();
-            boolean isExistEmail = memberRepository.existsByEmailAndIsDeletedFalse(email);
-            if (isExistEmail)
-                return SignupResponseDto.duplicateEmail();
-
-            String nickname = basicDto.getNickname();
-            boolean isExistNickname = memberRepository.existsByNicknameAndIsDeletedFalse(nickname);
-            if (isExistNickname)
-                return SignupResponseDto.duplicateNickname();
-
-            String password = basicDto.getPassword();
-            Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-            Matcher matcher = pattern.matcher(password);
-            if (!matcher.matches()) {
-                return SignupResponseDto.invalidPasswordPattern();
-            }
-
-            String encodedPassword = passwordEncoder.encode(password);
-            basicDto.setPassword(encodedPassword);
-
-            String certificationNumber = basicDto.getCertificationNumber();
-            boolean isCertificationNumberValid = emailCertificationRepository.existsByEmailAndCertificationNumber(email, certificationNumber);
-            if (!isCertificationNumberValid) {
-                return CheckCertificationResponseDto.certificationFail();
-            }
-
-            Member member = new Member(basicDto);
-            memberRepository.save(member);
-
-            Profile profile = new Profile(); // Member 객체 생성될 때 Profile 객체도 생성
-            profile.setMember(member);
-            profileRepository.save(profile);
-
-            token = jwtProvider.generateJwtToken(email);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.databaseError();
+    public String basicSignup(BasicSignupRequestDto basicDto) {
+        String email = basicDto.getEmail();
+        boolean isExistEmail = memberRepository.existsByEmailAndIsDeletedFalse(email);
+        if (isExistEmail) {
+            throw new ApiException(ErrorType._DUPLICATE_EMAIL);
         }
-        return SignupResponseDto.success(token);
+
+        String nickname = basicDto.getNickname();
+        boolean isExistNickname = memberRepository.existsByNicknameAndIsDeletedFalse(nickname);
+        if (isExistNickname) {
+            throw new ApiException(ErrorType._DUPLICATE_NICKNAME);
+        }
+
+        String password = basicDto.getPassword();
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.matches()) {
+            throw new ApiException(ErrorType.INVALID_PASSWORD_PATTERN);
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+        basicDto.setPassword(encodedPassword);
+
+        String certificationNumber = basicDto.getCertificationNumber();
+        boolean isCertificationNumberValid = emailCertificationRepository.existsByEmailAndCertificationNumber(email, certificationNumber);
+        if (!isCertificationNumberValid) {
+            throw new ApiException(ErrorType.CERTIFICATION_FAILED);
+        }
+
+        Member member = new Member(basicDto);
+        memberRepository.save(member);
+
+        Profile profile = new Profile(); // Member 객체 생성될 때 Profile 객체도 생성
+        profile.setMember(member);
+        profileRepository.save(profile);
+
+        return jwtProvider.generateJwtToken(email);
     }
 
     @Override
-    public ResponseEntity<? super SignupResponseDto> kakaoSignup(KakaoSignupRequestDto kakaoDto) {
-        return null; //이후에 구현
+    public String kakaoSignup(KakaoSignupRequestDto kakaoDto) {
+        // 이후에 구현
+        return null;
     }
 
     @Override
-    public ResponseEntity<? super UnRegisterResponseDto> unRegister(String email, UnRegisterRequestDto dto) {
-        try {
+    public UnRegisterResponseDto unRegister(String email, UnRegisterRequestDto dto) {
+        Member member = memberRepository.findByEmailAndIsDeletedFalse(email)
+                .orElseThrow(() -> new ApiException(ErrorType._USER_NOT_FOUND_DB));
 
-            Member member = memberRepository.findByEmailAndIsDeletedFalse(email);
-
-            if (member == null || member.isDeleted()) {
-                return UnRegisterResponseDto.memberNotFound();
-            }
-
-            boolean isMatched = passwordEncoder.matches(dto.getPassword(), member.getPassword());
-            if (!isMatched) {
-                return UnRegisterResponseDto.passwordMismatch();
-            }
-
-            member.markAsDeleted();
-            memberRepository.save(member);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.databaseError();
+        boolean isMatched = passwordEncoder.matches(dto.getPassword(), member.getPassword());
+        if (!isMatched) {
+            throw new ApiException(ErrorType._PASSWORD_MISMATCH);
         }
 
-        return UnRegisterResponseDto.success();
+        member.markAsDeleted();
+        memberRepository.save(member);
+        return null;
     }
-
-
 }
