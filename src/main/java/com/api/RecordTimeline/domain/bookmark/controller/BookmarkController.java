4a716eac294efef6766c2bb572c7a625d7b0490f
dhdbrud0510 <youkyungoh0510@naver.com>
2024-07-17 21:00:36 +0900

    feat: 반환 타입을 void로 유지하면서 ResponseDTO를 반환하도록 수정

--- src/main/java/com/api/RecordTimeline/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/com/api/RecordTimeline/domain/bookmark/controller/BookmarkController.java
@@ -1,7 +1,10 @@
 package com.api.RecordTimeline.domain.bookmark.controller;
 
+import com.api.RecordTimeline.domain.bookmark.dto.request.BookmarkRequestDTO;
+import com.api.RecordTimeline.domain.bookmark.dto.response.BookmarkResponseDTO;
 import com.api.RecordTimeline.domain.bookmark.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +16,8 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping("/{subTimelineId}")
-    public ResponseEntity<Void> toggleBookmark(@PathVariable Long subTimelineId) {
-        bookmarkService.toggleBookmark(subTimelineId);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> toggleBookmark(@RequestBody BookmarkRequestDTO bookmarkRequestDTO) {
+        bookmarkService.toggleBookmark(bookmarkRequestDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
