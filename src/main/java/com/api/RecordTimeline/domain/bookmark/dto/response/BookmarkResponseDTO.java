04a90944866d40243b11ce7555c21590dda717ef
dhdbrud0510 <youkyungoh0510@naver.com>
2024-07-17 21:01:40 +0900

    feat: 북마크 정보저장 ResponseDTO 생성

---
+++ src/main/java/com/api/RecordTimeline/domain/bookmark/dto/response/BookmarkResponseDTO.java
@@ -0,0 +1,10 @@
+package com.api.RecordTimeline.domain.bookmark.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BookmarkResponseDTO {
+    private String message;
+}
