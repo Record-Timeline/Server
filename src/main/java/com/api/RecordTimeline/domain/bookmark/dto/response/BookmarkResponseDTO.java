c3614649d5b06f37fff0394a7d1e64f16708b1f1
dhdbrud0510 <youkyungoh0510@naver.com>
2024-07-18 14:07:28 +0900

    feat: ResponseDTO에 성공과 실패 메세지 추가

--- src/main/java/com/api/RecordTimeline/domain/bookmark/dto/response/BookmarkResponseDTO.java
+++ src/main/java/com/api/RecordTimeline/domain/bookmark/dto/response/BookmarkResponseDTO.java
@@ -6,5 +6,17 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class BookmarkResponseDTO {
+서    private String code;
     private String message;
+    private Long bookmarkId;  // 북마크 ID를 포함하는 필드 추가
+
+    // 성공 시 호출되는 메서드
+    public static BookmarkResponseDTO success(Long bookmarkId) {
+        return new BookmarkResponseDTO("SU", "Success", bookmarkId);
+    }
+
+    // 실패 시 호출되는 메서드
+    public static BookmarkResponseDTO failure() {
+        return new BookmarkResponseDTO("UF", "Bookmark operation failed", null);
+    }
 }
