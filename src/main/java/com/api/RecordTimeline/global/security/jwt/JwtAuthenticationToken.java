00b52481f2fabc2b2f501c248e1b645d766635aa
hyeonbin <basilla0617@naver.com>
2024-07-19 15:45:28 +0900

    feat: JWT 토큰에서 사용자 ID와 이메일을 포함하는 인증 토큰 정의

---
+++ src/main/java/com/api/RecordTimeline/global/security/jwt/JwtAuthenticationToken.java
@@ -0,0 +1,31 @@
+package com.api.RecordTimeline.global.security.jwt;
+
+import lombok.Getter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import java.util.Collections;
+
+@Getter
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Long userId;
+    private final String email;
+    private final String token;
+
+    public JwtAuthenticationToken(Long userId, String email, String token) {
+        super(Collections.emptyList());
+        this.userId = userId;
+        this.email = email;
+        this.token = token;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
+}
