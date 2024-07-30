454ee3dd48a0dd9e4f40e89f0526000aea1c5448
hyeonbin <basilla0617@naver.com>
2024-07-30 14:02:31 +0900

    feat: 이메일 인증 목적 구분을 위한 필드 추가

--- src/main/java/com/api/RecordTimeline/domain/signup/email/domain/EmailCertification.java
+++ src/main/java/com/api/RecordTimeline/domain/signup/email/domain/EmailCertification.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Email;
 import lombok.*;
 
 @Getter
-@Builder
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailCertification {
@@ -17,8 +16,19 @@ public class EmailCertification {
 
     private String certificationNumber;
 
-    public EmailCertification(String email, String certificationNumber) {
+    @Column(nullable = false)
+    private String context; // SIGNUP, PASSWORD_RESET
+
+    @Column(nullable = false)
+    private boolean verified = false;
+
+    public void markAsVerified() {
+        this.verified = true;
+    }
+
+    public EmailCertification(String email, String certificationNumber, String context) {
         this.email = email;
         this.certificationNumber = certificationNumber;
+        this.context = context;
     }
 }
