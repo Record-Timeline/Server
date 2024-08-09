b186acecc9c7e8fad9d3e28955e1ed5fe833b9c7
dhdbrud0510 <youkyungoh0510@naver.com>
2024-08-09 19:10:39 +0900

    feat: 메인타임라인 조회 에러 해결하기 위해 파라미터 정의(@Param)

--- src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/mainTimeline/repository/MainTimelineRepository.java
@@ -3,6 +3,7 @@ package com.api.RecordTimeline.domain.mainTimeline.repository;
 import com.api.RecordTimeline.domain.mainTimeline.domain.MainTimeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +13,6 @@ public interface MainTimelineRepository extends JpaRepository<MainTimeline, Long
 
     // 특정 사용자의 메인 타임라인을 시작 날짜 기준으로 정렬하여 조회
     @Query("select m from MainTimeline m WHERE m.member.id = :memberId ORDER BY m.startDate")
-    List<MainTimeline> findByMemberIdOrderByStartDate(Long memberId);
+    List<MainTimeline> findByMemberIdOrderByStartDate(@Param("memberId") Long memberId);
 
 }
