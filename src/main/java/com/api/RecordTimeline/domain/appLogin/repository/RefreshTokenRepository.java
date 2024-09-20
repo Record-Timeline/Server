4593f7bdedaf047f5852d3af763d532278e1eb87
hyeonbin <basilla0617@naver.com>
2024-09-20 13:38:22 +0900

    feat: RefreshToken 엔티티 및 컨트롤러, 레파지토리 (#40)

---
+++ src/main/java/com/api/RecordTimeline/domain/appLogin/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.api.RecordTimeline.domain.appLogin.repository;
+
+import com.api.RecordTimeline.domain.appLogin.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByUserId(Long userId);
+
+    void deleteByUserId(Long userId);
+}
