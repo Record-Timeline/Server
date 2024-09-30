f313f93333805d34974fb82f9683fb6fa062be54
dhdbrud0510 <youkyungoh0510@naver.com>
2024-09-30 16:39:36 +0900

    feat: ReplyService에서 commentDTO를 replyDTO로 변경

--- src/main/java/com/api/RecordTimeline/domain/reply/service/ReplyService.java
+++ src/main/java/com/api/RecordTimeline/domain/reply/service/ReplyService.java
@@ -1,15 +1,15 @@
 package com.api.RecordTimeline.domain.reply.service;
 
 import com.api.RecordTimeline.domain.comment.domain.Comment;
-import com.api.RecordTimeline.domain.comment.dto.request.CommentUpdateRequestDTO;
-import com.api.RecordTimeline.domain.comment.dto.response.CommentDeleteResponseDTO;
-import com.api.RecordTimeline.domain.comment.dto.response.CommentUpdateResponseDTO;
 import com.api.RecordTimeline.domain.comment.repository.CommentRepository;
 import com.api.RecordTimeline.domain.member.domain.Member;
 import com.api.RecordTimeline.domain.member.repository.MemberRepository;
 import com.api.RecordTimeline.domain.reply.domain.Reply;
 import com.api.RecordTimeline.domain.reply.dto.request.ReplyCreateRequestDTO;
+import com.api.RecordTimeline.domain.reply.dto.request.ReplyUpdateRequestDTO;
+import com.api.RecordTimeline.domain.reply.dto.response.ReplyDeleteResponseDTO;
 import com.api.RecordTimeline.domain.reply.dto.response.ReplyResponseDTO;
+import com.api.RecordTimeline.domain.reply.dto.response.ReplyUpdateResponseDTO;
 import com.api.RecordTimeline.domain.reply.repository.ReplyRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -57,22 +57,22 @@ public class ReplyService {
                 .collect(Collectors.toList());
     }
 
-    public CommentUpdateResponseDTO updateComment(Long commentId, CommentUpdateRequestDTO requestDTO) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 댓글을 찾을 수 없습니다."));
+    public ReplyUpdateResponseDTO updateReply(Long replyId, ReplyUpdateRequestDTO requestDTO) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 대댓글을 찾을 수 없습니다."));
 
-        comment.setContent(requestDTO.getContent());  // 댓글 내용 업데이트
-        commentRepository.save(comment);
-        return CommentUpdateResponseDTO.success();  // 수정 성공 시 성공 응답 반환
+        reply.setContent(requestDTO.getContent());  // 대댓글 내용 업데이트
+        replyRepository.save(reply);
+        return ReplyUpdateResponseDTO.success();  // 수정 성공 시 성공 응답 반환
     }
 
     @Transactional
-    public CommentDeleteResponseDTO deleteComment(Long commentId) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 댓글을 찾을 수 없습니다."));
+    public ReplyDeleteResponseDTO deleteReply(Long replyId) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 대댓글을 찾을 수 없습니다."));
 
-        commentRepository.delete(comment);
-        return CommentDeleteResponseDTO.success();  // 삭제 성공 시 성공 응답 반환
+        replyRepository.delete(reply);
+        return ReplyDeleteResponseDTO.success();  // 삭제 성공 시 성공 응답 반환
     }
 }
 
