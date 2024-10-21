2e8c5e003c1880b758c24b556a0c2f7e6d7ac349
hyeonbin <basilla0617@naver.com>
2024-10-21 15:51:55 +0900

    feat: 만료 시간 지난 알림들은 DB에서 삭제

--- src/main/java/com/api/RecordTimeline/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/api/RecordTimeline/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,15 @@
 package com.api.RecordTimeline.domain.notification.repository;
 
 import com.api.RecordTimeline.domain.notification.domain.Notification;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByReceiverId(Long receiverId);  // 특정 사용자의 알림 목록 조회
+
+    @Transactional
+    void deleteByExpiryDateBefore(LocalDateTime expiryDate);
 }
