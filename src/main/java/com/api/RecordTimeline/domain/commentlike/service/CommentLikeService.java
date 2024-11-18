83c36208d7cb1b4c5675945329a01bf54e61870a
hyeonbin <basilla0617@naver.com>
2024-11-18 13:47:20 +0900

    feat: 대댓글 달림 실시간 알림 기능 구현 (#50)

--- src/main/java/com/api/RecordTimeline/domain/commentlike/service/CommentLikeService.java
+++ src/main/java/com/api/RecordTimeline/domain/commentlike/service/CommentLikeService.java
@@ -7,6 +7,9 @@ import com.api.RecordTimeline.domain.commentlike.repository.CommentLikeRepositor
 import com.api.RecordTimeline.domain.like.dto.response.LikeResponseDTO;
 import com.api.RecordTimeline.domain.member.domain.Member;
 import com.api.RecordTimeline.domain.member.repository.MemberRepository;
+import com.api.RecordTimeline.domain.notification.domain.NotificationType;
+import com.api.RecordTimeline.domain.notification.service.NotificationService;
+import com.api.RecordTimeline.domain.subTimeline.repository.SubTimelineRepository;
 import com.api.RecordTimeline.global.exception.ApiException;
 import com.api.RecordTimeline.global.exception.ErrorType;
 import com.api.RecordTimeline.global.security.jwt.JwtAuthenticationToken;
@@ -24,6 +27,7 @@ public class CommentLikeService {
 
     private final CommentLikeRepository commentLikeRepository;
     private final CommentRepository commentRepository;
+    private final NotificationService notificationService;
     private final MemberRepository memberRepository;
 
     @Transactional
@@ -43,9 +47,29 @@ public class CommentLikeService {
             commentLike.setComment(comment);
             commentLikeRepository.save(commentLike);
             comment.adjustLikeCount(1);
-            commentRepository.save(comment);
+
+            // 댓글 좋아요 알림
+            notificationService.sendNotification(
+                    member,
+                    comment.getMember(),
+                    member.getNickname() + "님이 당신의 댓글을 좋아합니다.",
+                    NotificationType.COMMENT_LIKE,
+                    comment.getId()
+            );
+
+            // 대댓글인 경우 게시글 작성자에게도 알림
+            if (comment.getSubTimeline() != null) {
+                notificationService.sendNotification(
+                        member,
+                        comment.getSubTimeline().getMainTimeline().getMember(),
+                        member.getNickname() + "님이 당신의 게시글의 대댓글을 좋아합니다.",
+                        NotificationType.REPLY_LIKE,
+                        comment.getId()
+                );
+            }
         }
 
+        commentRepository.save(comment);
         return new LikeResponseDTO("SU", "Success", comment.getLikeCount());
     }
 
